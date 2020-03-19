c21f486ef7fa5d89875c56238a685f0df619faa2
David Kim <kimbyi@bc.edu>
2020-03-19 19:01:22 -0400

    first commit

---
+++ poll/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .forms import CreatePollForm
+from .models import Poll
+# Create your views here.
+
+
+def home(request):
+    context ={}
+    return render(request, 'poll/home.html', context)
+def create(request):
+    form = CreatePollForm()
+    context ={
+        'form':form
+    }
+    return render(request, 'poll/create.html', context)
+def vote(request, poll_id):
+    context ={}
+    return render(request, 'poll/vote.html', context)
+def results(request, poll_id):
+    context ={}
+    return render(request, 'poll/results.html', context)
